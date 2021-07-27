6b2f7c884d9c5ce83cf67be34ab791c7e46e3f69
eswaribala <parameswaribala@gmail.com>
2021-07-27 15:45:02 +0530

    updated

--- customerapi/src/main/java/com/boa/customerapi/delegates/CustomerDelegate.java
+++ customerapi/src/main/java/com/boa/customerapi/delegates/CustomerDelegate.java
@@ -6,13 +6,26 @@ import java.time.format.DateTimeFormatter;
 
 import org.camunda.bpm.engine.delegate.DelegateExecution;
 import org.camunda.bpm.engine.delegate.JavaDelegate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
 
 import com.boa.customerapi.models.Customer;
 
+
+
 @Component("registrationDelegate")
 public class CustomerDelegate implements JavaDelegate{
-
+    @Autowired
+	private RestTemplate restTemplate;
+	@Value("${customerurl}")
+    private String url;     
+    
 	@Override
 	public void execute(DelegateExecution execution) throws Exception {
 		// TODO Auto-generated method stub
@@ -26,6 +39,17 @@ public class CustomerDelegate implements JavaDelegate{
 		LocalDate localDate=LocalDate.parse(date,formatter);
 		customer.setDob(localDate);
 		System.out.println(customer.getName());
+		//invoke rest service
+		HttpHeaders headers = new HttpHeaders();
+	       headers.setContentType(MediaType.APPLICATION_JSON);
+	    HttpEntity request = new HttpEntity<>(customer,headers);
+
+		ResponseEntity<?> response=restTemplate.
+	 		      postForEntity(url,request, String.class);
+
+		
+		
+		
 	}
 
 }
