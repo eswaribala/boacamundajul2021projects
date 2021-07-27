73ef405905b3c721861feb642da3038c1dcad9c5
eswaribala <parameswaribala@gmail.com>
2021-07-27 13:32:18 +0530

    updated

---
+++ customerapi/src/main/java/com/boa/customerapi/models/Customer.java
@@ -0,0 +1,32 @@
+package com.boa.customerapi.models;
+
+import java.time.LocalDate;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.format.annotation.DateTimeFormat.ISO;
+
+import lombok.Data;
+@Data
+@Entity
+@Table(name="Customer")
+public class Customer {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name="Customer_Id")
+	private long customerId;
+	@Column(name="Customer_Name")
+	private String name;
+	@Column(name="Mobile_No")
+	private long mobileNo;
+	@Column(name="DOB")
+	@DateTimeFormat(iso = ISO.DATE)
+	private LocalDate dob;
+
+}
